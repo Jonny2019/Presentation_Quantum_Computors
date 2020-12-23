d8c17a8caff038a95450b88d19022a4b5dc56d16
Jonny2019 <justus.bendel@live.de>
2020-12-23 12:21:28 +0000

    extention of problems

--- test.py
+++ test.py
@@ -2,13 +2,31 @@ from dwave.system import EmbeddingComposite, DWaveSampler
 
 # Define the problem as two Python dictionaries: 
 #   h for linear terms, J for quadratic terms
-h = {} 
+
+
+# Kreuzung einfach
+h = {0,0,0,0}
 J = {
-    (0,1): 1,
-    (1,3): 1,
-    (3,2): 1,
-    (2,0): 1
-} 
+    (0, 1): 1,
+    (0, 3): 1,
+    (1, 2): 1,
+    (2, 3): 1
+}
+
+# Schachbrett 3 x 3
+h = {0,0,0,0}
+J = {
+    (0, 1): 1,
+    (0, 3): 1,
+    (1, 2): 1,
+    (1, 4): 1,
+    (2, 5): 1,
+    (3, 4): 1,
+    (3, 6): 1,
+    (4, 5): 1,
+    (4, 7): 1,
+    (5, 8): 1
+}
 
 # Define the sampler that will be used to run the problem
 sampler = EmbeddingComposite(DWaveSampler())
